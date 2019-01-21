579c4226a200f0b1b6ee58767f672fd4d62f4962
Evan Stoll <evanjsx@gmail.com>
2019-01-21 09:09:46 -0500

    Trim x.nix

--- x.nix
+++ x.nix
@@ -3,43 +3,13 @@
 
   environment.systemPackages = with pkgs; [
     arandr
-    #(xmonad-with-packages.override {
-      #packages = self: with self; [
-        #taffybar
-        #xmonad-contrib
-        #xmonad-extras
-        #xmobar
-      #];
-    #})
-    (haskellPackages.ghcWithPackages (self: with self; [
-      mtl
-      xmonad
-      xmonad-contrib
-      xmonad-extras
-      xmobar
-      taffybar
-      cabal-install
-      lens
-      curl
-
-      # taffybar
-      gi-atk
-      gi-cairo
-      gi-dbusmenu
-      gi-dbusmenugtk3
-      gi-gdk
-      gi-gdkpixbuf
-      gi-gio
-      gi-glib
-      gi-gobject
-      gi-gtk
-      gi-gtk-hs
-      gi-pango
-      gi-xlib
-      gtk-sni-tray
-      gtk-strut
-      haskell-gi
-    ]))
+    (xmonad-with-packages.override {
+      packages = self: with self; [
+        xmonad-contrib
+        xmonad-extras
+        haskellPackages.xmobar
+      ];
+    })
   ];
   services.xserver = {
     enable = true;
