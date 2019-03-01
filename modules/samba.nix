1ff1b7a1622d4c90ae39f194c3bf19eee69092f9
Evan Stoll <evanjsx@gmail.com>
2019-03-01 00:01:28 -0500

    Add dolphin plugins to samba module

--- modules/samba.nix
+++ modules/samba.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    kdeApplications.kdenetwork-filesharing
+    kdeApplications.dolphin-plugins
+    kdeFrameworks.kio
+    kdeApplications.kio-extras
+    dolphin
+  ];
+
   services.samba = {
     enable = true;
     configText = ''
