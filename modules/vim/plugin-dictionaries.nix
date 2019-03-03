1582612250bf409bddc195800553e8982612b8b7
Evan Stoll <evanjsx@gmail.com>
2019-03-03 01:10:39 -0500

    Use polyglot instead of multiple language plugins for vim

--- modules/vim/plugin-dictionaries.nix
+++ modules/vim/plugin-dictionaries.nix
@@ -1,14 +1,9 @@
 [
-  { name  =  "YouCompleteMe"; }
-
-  { names = [ "nerdcommenter" "vim-autoformat" "vim-airline" ]; }
-  { name  = "LanguageClient-neovim"; }
+  { name = "airline" ; }
   { name = "fugitive"; }
-
-  # Several plugins do their own ft/filename detection; always load those plugins so they can take care of things
-  { name = "vim-toml"; }
-  { name = "vim-qml"; }
-  { name = "haskell-vim"; }
-  { name = "vim-nix"; }
-  { name = "Jenkinsfile-vim-syntax"; }
+  { name  = "LanguageClient-neovim"; }
+  { name = "nerdcommenter"; }
+  { name = "polyglot"; }
+  { name = "vim-autoformat"; }
+  { name  =  "YouCompleteMe"; }
 ]
