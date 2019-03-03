8025f3c758532789b8cb60436749193bd3ab0cbc
Evan Stoll <evanjsx@gmail.com>
2019-03-02 20:53:58 -0500

    Unconditionally enable most vim plugins to delegate ftdetect and related functions

--- modules/vim/plugin-dictionaries.nix
+++ modules/vim/plugin-dictionaries.nix
@@ -1,21 +1,14 @@
 [
-  { name  =  "youcompleteme"; }
+  { name  =  "YouCompleteMe"; }
+
   { names = [ "nerdcommenter" "vim-autoformat" "vim-airline" ]; }
   { name  = "LanguageClient-neovim"; }
   { name = "fugitive"; }
 
-  # only load for TOML files
-  { name = "vim-toml"; filename_regex = "^\\.toml$"; }
-
-  # only load for QML files
-  { name = "vim-qml"; filename_regex  = "^\\.qml$"; }
-
-  # only load for Haskell files
-  { name = "haskell-vim"; filename_regex = "^\\.hs$"; }
-
-  # only load for Nix files
-  { name = "vim-nix"; filename_regex = "^\\.nix$"; }
-
-  # only load for Jenkinsfiles
-  { name = "Jenkinsfile-vim-syntax"; filename_regex = "Jenkinsfile"; }
+  # Several plugins do their own ft/filename detection; always load those plugins so they can take care of things
+  { name = "vim-toml"; }
+  { name = "vim-qml"; }
+  { name = "haskell-vim"; }
+  { name = "vim-nix"; }
+  { name = "Jenkinsfile-vim-syntax"; }
 ]
