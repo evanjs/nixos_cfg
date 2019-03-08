f1637de4fdf6496a2cb041a03164346c88f1edce
Evan Stoll <evanjsx@gmail.com>
2019-03-08 11:12:26 -0500

    Sort packages in evanjs overlay

--- overlays/evanjs/overlay.nix
+++ overlays/evanjs/overlay.nix
@@ -4,15 +4,15 @@ let
   callQPackage = self.libsForQt5.callPackage;
 in 
   {
+    booru = callPackage ./pkgs/booru { };
     cross = callPackage ./pkgs/cross { };
+    exercism = callPackage ./pkgs/tools/exercism { };
     kivy = callPackage ./pkgs/python/kivy { python = self.python37; };
     kivy-garden = callPackage ./pkgs/python/kivy-garden { python = self.python37; };
-    exercism = callPackage ./pkgs/tools/exercism { };
-    runescape-launcher = callPackage ./pkgs/runescape-launcher { };
-    sccache = callPackage ./pkgs/tools/sccache { };
-    rrbg = callPackage ./pkgs/rrbg { };
     polychromatic = callPackage ./pkgs/polychromatic { };
-    rust-qt-binding-generator = callQPackage ./pkgs/rust-qt-binding-generator { };
-    booru = callPackage ./pkgs/booru { };
     qrbooru = callQPackage ./pkgs/qrbooru { };
+    rrbg = callPackage ./pkgs/rrbg { };
+    runescape-launcher = callPackage ./pkgs/runescape-launcher { };
+    rust-qt-binding-generator = callQPackage ./pkgs/rust-qt-binding-generator { };
+    sccache = callPackage ./pkgs/tools/sccache { };
   }
