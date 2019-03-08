88f84fdf53353a3850b21f1f0a25956c59ddb631
Evan Stoll <evanjsx@gmail.com>
2019-03-08 11:11:46 -0500

    Use context menu-centered hotkeys for LanguageClient

--- modules/vim/rc.nix
+++ modules/vim/rc.nix
@@ -51,18 +51,18 @@ in
 
     " let g:ycm_python_binary_path = '${pkgs.python37}/bin/python3'
 
-    "nnoremap <F5>  :call LanguageClient_contextMenu()<CR>
-    "nnoremap <silent> K :call LanguageClient#textDocument_hover()<CR>
-    "nnoremap <silent> gd :call LanguageClient#textDocument_definition()<CR>
-    "nnoremap <silent> <F6> :call LanguageClient#textDocument_rename()<CR>
+    nnoremap <F5>  :call LanguageClient_contextMenu()<CR>
+    nnoremap <silent> K :call LanguageClient#textDocument_hover()<CR>
+    nnoremap <silent> gd :call LanguageClient#textDocument_definition()<CR>
+    nnoremap <silent> <F6> :call LanguageClient#textDocument_rename()<CR>
 
-    map <Leader>lk :call LanguageClient#textDocument_hover()<CR>
-    map <Leader>lg :call LanguageClient#textDocument_definition(<CR>
-    map <Leader>lr :call LanguageClient#textDocument_rename()<CR>
-    map <Leader>lf :call LanguageClient#textDocument_formatting()<CR>
-    map <Leader>lb :call LanguageClient#textDocument_references()<CR>
-    map <Leader>la :call LanguageClient#textDocument_codeAction()<CR>
-    map <Leader>ls :call LanguageClient#textDocument_documentSymbol()<CR>
+    "map <Leader>lk :call LanguageClient#textDocument_hover()<CR>
+    "map <Leader>lg :call LanguageClient#textDocument_definition(<CR>
+    "map <Leader>lr :call LanguageClient#textDocument_rename()<CR>
+    "map <Leader>lf :call LanguageClient#textDocument_formatting()<CR>
+    "map <Leader>lb :call LanguageClient#textDocument_references()<CR>
+    "map <Leader>la :call LanguageClient#textDocument_codeAction()<CR>
+    "map <Leader>ls :call LanguageClient#textDocument_documentSymbol()<CR>
 
     "if has ("autocmd")
     "  augroup templates
