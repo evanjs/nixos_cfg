393b703e58d056d788080312e99c4a6be14012e7
Evan Stoll <evanjsx@gmail.com>
2019-03-18 09:29:44 -0400

    Add authentication configuration to postgresql module

--- modules/db/postgresql.nix
+++ modules/db/postgresql.nix
@@ -7,6 +7,19 @@
     };
 
     postgresql = {
+      authentication = pkgs.lib.mkOverride 10 ''
+        local   all             all                                     trust
+        host    all             all             127.0.0.1/32            trust
+        host    all             all             ::1/128                 trust
+        local   replication     all                                     trust
+        host    replication     all             127.0.0.1/32            trust
+        host    replication     all             ::1/128                 trust
+      '';
+
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE DB copilot
+      '';
+
       enable = true;
       enableTCPIP = true;
       package = pkgs.postgresql_10;
