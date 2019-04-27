8c39bdb08f92ab6733f5618dd5ab180815996efd
Evan Stoll <evanjsx@gmail.com>
2019-04-27 16:53:23 -0400

    Fix formatting for fakue-useragent expression

--- overlays/evanjs/pkgs/python/fake-useragent/default.nix
+++ overlays/evanjs/pkgs/python/fake-useragent/default.nix
@@ -12,10 +12,10 @@ in
       version = "0.1.11";
 
       src = fetchFromGitHub {
-	owner = "hellysmile";
-	repo = "fake-useragent";
-	rev = "a55c4dc294883c73231e085fca34ab5a8e2bc1f1";
-	sha256 = "01b0zwd4bvxmc8gwd859sns2f953ns0gygkkv7m447x6vdsymzkj";
+        owner = "hellysmile";
+        repo = "fake-useragent";
+        rev = "a55c4dc294883c73231e085fca34ab5a8e2bc1f1";
+        sha256 = "01b0zwd4bvxmc8gwd859sns2f953ns0gygkkv7m447x6vdsymzkj";
       };
 
       propagatedBuildInputs = builtins.attrValues python.packages;
