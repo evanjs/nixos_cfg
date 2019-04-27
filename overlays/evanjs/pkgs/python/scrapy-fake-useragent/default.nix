ac7196227619b4d75e8f80580bb79956efcc1d1a
Evan Stoll <evanjsx@gmail.com>
2019-04-27 16:43:39 -0400

    Fix formatting for scrapy-fake-useragent; disable doCheck

--- overlays/evanjs/pkgs/python/scrapy-fake-useragent/default.nix
+++ overlays/evanjs/pkgs/python/scrapy-fake-useragent/default.nix
@@ -11,14 +11,12 @@ in
       name = "scrapy-fake-useragent";
 
       src = fetchFromGitHub {
-	owner = "alecxe";
-	repo = "scrapy-fake-useragent";
+        owner = "alecxe";
+        repo = "scrapy-fake-useragent";
         rev = "d9ab527b7b4dcf2430095ab91c23885190e7b084";
         sha256 = "03il4jdrpzsnqacn13df58lz0hnzny9l6kb56gzl5dprdpkpry1m";
       };
 
       propagatedBuildInputs = builtins.attrValues python.packages;
-
-      doCheck = false;
     };
   }
