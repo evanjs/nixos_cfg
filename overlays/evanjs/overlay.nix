9caf1d5c6af884c89537d56810fa0079c7a35954
Evan Stoll <evanjsx@gmail.com>
2019-04-27 22:15:45 -0400

    Add openssl override and patched libwebsockets

--- overlays/evanjs/overlay.nix
+++ overlays/evanjs/overlay.nix
@@ -1,15 +1,15 @@
 self: super:
 let
   callPackage = self.callPackage;
+  callPackages = self.callPackages;
   callQPackage = self.libsForQt5.callPackage;
 in 
   {
     booru = callPackage ./pkgs/booru { };
     cross = callPackage ./pkgs/cross { };
     exercism = callPackage ./pkgs/tools/exercism { };
-    kivy = callPackage ./pkgs/python/kivy { python = self.python37; };
-    kivy-garden = callPackage ./pkgs/python/kivy-garden { python = self.python37; };
-    polychromatic = callPackage ./pkgs/polychromatic { };
+    copenssl = callPackages ./pkgs/openssl { };
+    libwebsockets = callPackage ./pkgs/libwebsockets { openssl = self.copenssl.openssl_1_1; };
     qrbooru = callQPackage ./pkgs/qrbooru { };
     rrbg = callPackage ./pkgs/rrbg { };
     runescape-launcher = callPackage ./pkgs/runescape-launcher { };
