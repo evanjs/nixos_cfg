475219ba518c2fd243f302aec188c96ef9e93993
Evan Stoll <evan.stoll@rjginc.com>
2019-05-03 11:48:51 -0400

    Disable virtualbox for work laptop

--- machines/work/default.nix
+++ machines/work/default.nix
@@ -8,7 +8,7 @@
     ../../modules/android.nix
     ../../modules/db/postgresql.nix
     ../../modules/development.nix
-    ../../modules/virtualization/virtualbox.nix
+    #../../modules/virtualization/virtualbox.nix # might re-enable after building / pushing to cachix from rig
   ];
 
   #nixpkgs.config.allowUnfree = true;
