56faa98d6cf3f23fb928f3282aee94eb6315f69d
Evan Stoll <evanjsx@gmail.com>
2019-05-07 13:54:39 -0400

    Switch back to nixpkgs-unstable

--- overlays/evanjs/overlay.nix
+++ overlays/evanjs/overlay.nix
@@ -12,16 +12,16 @@ in
   {
     booru = callPackage ./pkgs/booru { };
     copenssl = callPackages ./pkgs/openssl { };
-    cross = callPackage ./pkgs/cross { rustPlatform = unstableRustPlatform; };
+    cross = callPackage ./pkgs/cross { };
     exercism = callPackage ./pkgs/tools/exercism { };
     kivy = callPackage ./pkgs/python/kivy { python = self.python37; };
     kivy-garden = callPackage ./pkgs/python/kivy-garden { python = self.python37; };
     kivymd = callPackage ./pkgs/python/kivymd { python = self.python37; };
     material-ui = callPackage ./pkgs/python/material-ui { python = self.python37; };
-    nget = callPackage ./pkgs/nget { rustPlatform = unstableRustPlatform; };
+    nget = callPackage ./pkgs/nget { };
     polychromatic = callPackage ./pkgs/polychromatic { };
     qrbooru = callQPackage ./pkgs/qrbooru { };
-    rrbg = callPackage ./pkgs/rrbg { rustPlatform = unstableRustPlatform; };
+    rrbg = callPackage ./pkgs/rrbg { };
     runescape-launcher = callPackage ./pkgs/runescape-launcher { };
     rust-qt-binding-generator = callQPackage ./pkgs/rust-qt-binding-generator { };
     sccache = callPackage ./pkgs/tools/sccache { };
