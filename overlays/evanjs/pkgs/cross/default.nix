e34f4cb0d12f0a23588f56b8ed8aab54b8e6af68
Evan Stoll <evan.stoll@rjginc.com>
2019-05-08 14:32:56 -0400

    Update cross cargoSha256o

--- overlays/evanjs/pkgs/cross/default.nix
+++ overlays/evanjs/pkgs/cross/default.nix
@@ -17,7 +17,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1bfm8f8z5818bjaxff6jzfa28v844vjd2ldl6g1q82wl7iwrxbqk";
   };
 
-  cargoSha256 = "1va2fbkiq96m3h2jahc0avdzndgg82giln2llbmgvkhbml0y1lrn";
+  cargoSha256 = "0y2nllc7k2j5ia9w69wnba6vr4l6hagsf5yjny761hz3w027wagm";
 
   nativeBuildInputs = [
     libressl
