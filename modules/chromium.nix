4bc7e8a88479dfebd73662953a87310e1b07ae2a
Evan Stoll <evanjsx@gmail.com>
2019-05-11 15:59:02 -0400

    Revert "Enable programs.chromium instead of adding to systemPackages"
    This reverts commit fc4ed47ae9e557603bfbf8a20d01517882c6f9d9.

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ...}:
 {
+  environment.systemPackages = with pkgs; [
+    chromium
+  ];
+
   programs.chromium = {
-    enable = true;
     extensions = [
       "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password X
       "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
