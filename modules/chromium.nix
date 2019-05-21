be9df9555997bc2e62f354bb45eed995b54936b1
Evan Stoll <evan.stoll@rjginc.com>
2019-05-21 14:15:10 -0400

    Add enablePepperFlash to Chromium for flash

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -4,6 +4,12 @@
     chromium
   ];
 
+  nixpkgs.config = {
+    chromium = {
+      enablePepperFlash = true; # Chromium removed support for Mozilla (NPAPI) plugins so Adobe Flash no longer works
+    };
+  };
+
   programs.chromium = {
     extensions = [
       "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password X
