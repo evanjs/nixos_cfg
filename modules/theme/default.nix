41519373cb7dc50bca7455a8ceb14996cb04cb92
Evan Stoll <evanjsx@gmail.com>
2019-05-26 14:29:45 -0400

    Add breeze-icons to theme module

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -5,7 +5,7 @@
   ];
 
   environment = {
-    systemPackages = with pkgs; [ breeze-gtk breeze-qt5 ] ++ [ lxappearance ];
+    systemPackages = with pkgs; [ breeze-gtk breeze-qt5 breeze-icons ] ++ [ lxappearance ];
     shellInit = ''
         export GTK_PATH=$GTK_PATH:${pkgs.breeze-gtk}/share/themes/Breeze-Dark/gtk-2.0
         export GTK2_RC_FILES=${pkgs.breeze-gtk}/share/themes/Breeze-Dark/gtk-2.0/gtkrc
