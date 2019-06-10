a81f24fe462fa2838584a564640b5454124c2b8f
Evan Stoll <evan.stoll@rjginc.com>
2019-06-10 12:06:51 -0400

    Update samba client config for work with mount for RJGFS1

--- modules/samba/client/work.nix
+++ modules/samba/client/work.nix
@@ -13,8 +13,23 @@
   ];
 
   services.samba = {
-    configText = ''
-        workgroup = RJGTECH.LOCAL
+    extraConfig = ''
+      local master = yes
+      workgroup = RJGTECH.LOCAL
+      ;client use spnego = no
+      ;client ntlmv2 auth = no
     '';
   };
+
+
+  fileSystems."/mnt/rjg/rjgfs1/engineering" = {
+    device = "//RJGFS1/Engineering";
+    fsType = "cifs";
+    options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,ip=192.168.2.2";
+
+    in ["${automount_opts},credentials=/home/evanjs/src/nixos_cfg/modules/samba/client/smb-secrets"];
+  };
 }
+
