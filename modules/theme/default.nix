62a1ff9e60d1a7b8281b350902ae706996483525
Evan Stoll <evanjsx@gmail.com>
2019-06-10 08:36:45 -0400

    Add hicolor-icon-theme to theme module systemPackages

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -5,7 +5,7 @@
   ];
 
   environment = {
-    systemPackages = with pkgs; [ breeze-gtk breeze-qt5 breeze-icons ] ++ [ lxappearance ];
+    systemPackages = with pkgs; [ breeze-gtk breeze-qt5 breeze-icons hicolor-icon-theme ] ++ [ lxappearance ];
     shellInit = ''
         export GTK_PATH=$GTK_PATH:${pkgs.breeze-gtk}/share/themes/Breeze-Dark/gtk-2.0
         export GTK2_RC_FILES=${pkgs.breeze-gtk}/share/themes/Breeze-Dark/gtk-2.0/gtkrc
