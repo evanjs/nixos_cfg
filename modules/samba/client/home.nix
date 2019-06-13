1f5169320d943a28f73ba312a5340fe1f72c7c22
Evan Stoll <evan.stoll@rjginc.com>
2019-06-12 22:25:58 -0400

    Add home config for samba client

---
+++ modules/samba/client/home.nix
@@ -0,0 +1,16 @@
+{ config, ... }:
+{
+  imports = [
+    ./default.nix
+  ];
+
+  fileSystems."/data/stores/" = {
+    device = "//rig/private";
+    fsType = "cifs";
+    options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/home/evanjs/src/nixos_cfg/modules/samba/client/smb-secrets"];
+  };
+}
