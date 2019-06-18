8cf6b84c7834b11d34bf35c9652ee59d13ec30ec
Evan Stoll <evan.stoll@rjginc.com>
2019-06-18 19:37:47 -0400

    Disable powertop module

--- modules/power/powertop.nix
+++ modules/power/powertop.nix
@@ -6,7 +6,13 @@
 
   powerManagement = {
     powertop = {
-      enable = true;
+      /*
+      * currently, powertop does not have any (nixos) options, and does not base its behavior
+      * on the power source, so Autosuspend is always on, etc
+      * disable until this can be configured
+      * e.g. Do not autosuspend USB devices if connected to AC power
+      */
+      enable = false;
     };
   };
 }
