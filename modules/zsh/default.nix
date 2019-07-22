4e3cf091c045b6f49a17d28572b6673708ec1f56
Evan Stoll <evanjsx@gmail.com>
2019-07-22 10:32:21 -0400

    Enable zsh program in zsh module

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -4,6 +4,10 @@
     pathsToLink = [ "/share/zsh" ];
   };
 
+  programs.zsh = {
+    enable = true;
+  };
+
   # enable completion for system packages (e.g. systemd)
   users.users.evanjs.shell = pkgs.zsh;
   
