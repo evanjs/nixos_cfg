d14ab1c0be8380654e527fbf84c34cbe38fd5b87
Evan Stoll <evan.stoll@rjginc.com>
2019-08-05 14:27:04 -0400

    Force home-manager fontconfig.enable to true to silence conflicting definition errors

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,6 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   home-manager.users.evanjs = {
+    fonts = {
+      fontconfig = {
+        enable = lib.mkForce true;
+      };
+    };
     home.packages = with pkgs; [
       nerdfonts
       powerline-fonts
