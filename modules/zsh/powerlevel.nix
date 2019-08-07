6f9413b767048d6bdc17e650e4e4872eda6b7cea
Evan Stoll <evan.stoll@rjginc.com>
2019-08-07 15:47:17 -0400

    Use promptInit instead of interactiveShellInit for powerlevel9k

--- modules/zsh/powerlevel.nix
+++ modules/zsh/powerlevel.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   programs.zsh = {
-    interactiveShellInit = ''
+    promptInit = ''
       source ${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme
     '';
   };
