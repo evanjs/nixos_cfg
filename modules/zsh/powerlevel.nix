8ae983d8f4a283b9c21bcbb6bbd8b457e1705858
Evan Stoll <evan.stoll@rjginc.com>
2019-08-07 17:16:12 -0400

    Set POWERLEVEL9K_MODE in programs.zsh.promptInit

--- modules/zsh/powerlevel.nix
+++ modules/zsh/powerlevel.nix
@@ -2,6 +2,7 @@
 {
   programs.zsh = {
     promptInit = ''
+      POWERLEVEL9K_MODE="nerdfont-complete";
       source ${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme
     '';
   };
