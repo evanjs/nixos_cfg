9f053c5da421ae8fb3d895bbc8f33cc282939f40
Evan Stoll <evan.stoll@rjginc.com>
2019-08-19 16:12:35 -0400

    config: add nix module and autoPull service

--- config/default.nix
+++ config/default.nix
@@ -4,6 +4,7 @@
     /* ./assets */
     ./new-modules
     /* ./machines/connectivity.nix */
+    ./nix.nix
     ./personal
     ./pkgs/module.nix
     ./cachix.nix
---
+++ config/nix.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, options, lib, ... }:
+{
+  imports = [
+    ../services/autoPull.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nix-index
+    nix-prefetch-scripts
+  ];
+
+  # Workaround for missing gz functionality in auto-upgrade packages
+  # For more information, see https://github.com/NixOS/nixpkgs/issues/28527#issuecomment-325182680
+  systemd.services.nixos-upgrade.path = with pkgs; [  gnutar xz.bin gzip config.nix.package.out ];
+
+  system = {
+    autoUpgrade = {
+      channel = "nixos";
+      dates = "00/6:00";
+      enable = true;
+    };
+    copySystemConfiguration = true;
+    extraSystemBuilderCmds = ''
+      ln -sv ${../.} $out/nixcfg
+    '';
+  };
+
+  nix = {
+    autoOptimiseStore = true;
+    gc = {
+      automatic = false;
+      dates = "04:00";
+    };
+
+    extraOptions = ''
+      min-free = ${toString (1024 * 1024 * 1024)}
+    '';
+  };
+}
