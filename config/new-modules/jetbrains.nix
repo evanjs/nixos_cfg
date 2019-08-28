81a6e975e57974ce130043fa814855fd1ac708c6
Evan Stoll <evan.stoll@rjginc.com>
2019-08-28 10:31:23 -0400

    jetbrains: add ideSupport option

--- config/new-modules/jetbrains.nix
+++ config/new-modules/jetbrains.nix
@@ -20,14 +20,39 @@ in
         description = "The JetBrains packages to install";
       };
       useLatest = mkEnableOption "using latest JetBrains programs";
+      ideSupport = {
+        enable = mkOption {
+          type = types.bool;
+          default = true;
+          example = false;
+          description = "JetBrains IDE Support for Chrome-based browsers";
+        };
+        port = mkOption {
+          type = types.int;
+          default = 63342;
+          example = 8080;
+          description = "The port to use to connect to the IDE";
+        };
+        openFirewall = mkOption {
+          type = types.bool;
+          default = true;
+          example = false;
+          description = "Open ports in the firewall for JetBrains IDE support.  This is required for remote debugging.";
+        };
+      };
     };
 
     config = mkIf cfg.enable {
-      mine.userConfig = {
-        home.packages =
-          if cfg.useLatest 
-          then lists.map getNewestFromChannels cfg.packages
-          else cfg.packages;
-      };
-    };
-  }
+      networking.firewall.allowedTCPPorts =
+        if cfg.ideSupport.enable
+        then [ cfg.ideSupport.port ]
+        else [];
+
+        mine.userConfig = {
+          home.packages =
+            if cfg.useLatest
+            then lists.map getNewestFromChannels cfg.packages
+            else cfg.packages;
+          };
+        };
+      }
