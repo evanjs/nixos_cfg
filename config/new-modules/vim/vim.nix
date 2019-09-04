de8eee8ca9273a8ec15fda728fad1588c16be2d7
Evan Stoll <evanjsx@gmail.com>
2019-09-03 23:08:41 -0400

    neovim: use plugins instead of configure

--- config/new-modules/vim/vim.nix
+++ config/new-modules/vim/vim.nix
@@ -7,44 +7,37 @@ let
 
 in
 
-{
-  options.mine.vim.enable = mkEnableOption "vim config";
-  config.mine.userConfig = mkIf config.mine.vim.enable {
+  {
+    options.mine.vim.enable = mkEnableOption "vim config";
+    config.mine.userConfig = mkIf config.mine.vim.enable {
 
-    nixpkgs.overlays = [(self: super: {
-    })];
+      nixpkgs.overlays = [(self: super: {
+      })];
 
-    programs.neovim = {
-      enable = true;
+      programs.neovim = {
+        enable = true;
 
-      viAlias = true;
-      vimAlias = true;
-      withPython3 = true;
+        viAlias = true;
+        vimAlias = true;
+        withPython3 = true;
 
-      configure = {
-        ##customRC = (toString (toString (builtins.attrValues (pkgs.nix-helpers.importFrom ./config))));
-        vam = {
-          knownPlugins = pkgs.vimPlugins;
-          pluginDictionaries =
-            [
-              { name = "fugitive"; }
-              { name = "ghc-mod-vim"; }
-              { name = "haskell-vim"; }
-              { name  = "LanguageClient-neovim"; }
-              { name = "nerdcommenter"; }
-              { name = "neomake"; }
-              { name = "polyglot"; }
-              { name = "ranger-vim"; }
-              { name = "rust-vim"; }
-              { name = "SpaceCamp"; }
-              { name = "syntastic"; }
-              { name = "vim-airline"; }
-              { name = "vim-airline-themes"; }
-              { name = "vim-autoformat"; }
-              { name  =  "YouCompleteMe"; }
-            ];
-          };
-        };
+        plugins = with pkgs.vimPlugins; [
+          fugitive
+          ghc-mod-vim
+          haskell-vim
+          LanguageClient-neovim
+          nerdcommenter
+          neomake
+          polyglot
+          ranger-vim
+          rust-vim
+          SpaceCamp
+          syntastic
+          vim-airline
+          vim-airline-themes
+          vim-autoformat
+          YouCompleteMe
+        ];
       };
     };
   }
