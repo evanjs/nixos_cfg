693167e7db8b2b128688ee00ea90358ce06cefd1
Evan.Stoll <evan.stoll@rjginc.com>
2019-09-06 19:24:01 -0400

    sources: update nur

--- config/sources/nur.nix
+++ config/sources/nur.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "nix-community";
   repo = "NUR";
-  rev = "e450a12845bd172022f855a1fb26b7715ac2f22f";
-  sha256 = "1ss6d21y07did6vmksp87952n6za699hycz477gn2k5d6yrmdwxx";
+  rev = "d904bd755d932c286fc2fa042e21a7faf403011e";
+  sha256 = "06cpqg55vilraixw3a6dnvhxmf7km2imp1v2avnvpg5w3fr3h3a1";
 }
