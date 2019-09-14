a1ed0077bcfd6fe9c834bb6464c99c606293bace
Evan.Stoll <evan.stoll@rjginc.com>
2019-09-14 16:29:11 -0400

    profiles/laptop: add laptop profile from nixos-hardware
    - sources: add nixos-hardware

---
+++ config/sources/nixos-hardware.nix
@@ -0,0 +1,6 @@
+{ pkgs }: pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixos-hardware";
+  rev = "043562906168ee6966b6409c89a2b219af1e9752";
+  sha256 = "060ggnrlcdsmzwx8yql005y3m5l3pr4sfdhq744s64b22cs5a739";
+}
