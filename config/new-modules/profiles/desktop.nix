0af9ee27c79c5c4b12a8e178e2addd9e8152b83e
Evan Stoll <evan.stoll@rjginc.com>
2019-09-21 23:10:19 -0400

    profiles/desktop: add cargo-watch to dev.rust.extraPackages

--- config/new-modules/profiles/desktop.nix
+++ config/new-modules/profiles/desktop.nix
@@ -42,7 +42,7 @@ in
       enable = true;
       plugins = [ "rust-std" "rust-src"];
       channel = "nightly";
-      extraPackages = with pkgs; [ cargo-edit cargo-license cargo-asm cargo-outdated cargo-update cargo-bloat cargo-fuzz openssl pkgconfig stdenv.cc sccache ];
+      extraPackages = with pkgs; [ cargo-edit cargo-license cargo-asm cargo-outdated cargo-update cargo-bloat cargo-fuzz cargo-watch openssl pkgconfig stdenv.cc sccache ];
     };
     mine.jetbrains = {
       enable = true;
