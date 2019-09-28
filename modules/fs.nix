92c88d29fb435ba21359a294b2fefad38c3febc6
Evan Stoll <evan.stoll@rjginc.com>
2019-09-28 15:25:39 -0400

    fs: remove no longer necessary exFAT patch

--- modules/fs.nix
+++ modules/fs.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }:
-let
-  exfat-nofuse = config.boot.kernelPackages.exfat-nofuse.overrideAttrs (attrs: {
-      patches = attrs.patches ++ [ /etc/nixos/patches/exfat/MS_toSB_macros.patch ];
-
-    });
-in
 {
   boot.extraModulePackages = [
-    #config.boot.kernelPackages.exfat-nofuse 
-    exfat-nofuse
+    config.boot.kernelPackages.exfat-nofuse
   ];
 }
