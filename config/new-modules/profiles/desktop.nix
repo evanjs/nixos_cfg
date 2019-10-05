dba03d2c60f98b1a5d6a49df47959891ad459e5b
Evan Stoll <evan.stoll@rjginc.com>
2019-10-05 17:03:58 -0400

    dev/rust: remove rustfmt-preview from main rust package

--- config/new-modules/profiles/desktop.nix
+++ config/new-modules/profiles/desktop.nix
@@ -39,7 +39,7 @@ in
     mine.dev.haskell.enable = true;
     mine.dev.rust = {
       enable = true;
-      plugins = [ "rust-std" "rust-src" "rustfmt-preview"];
+      plugins = [ "rust-std" "rust-src" ];
       channel = "nightly";
       extraPackages = with pkgs; [ cargo-edit cargo-license cargo-asm cargo-outdated cargo-update cargo-bloat cargo-fuzz cargo-watch openssl pkgconfig stdenv.cc sccache pkgs.nixpkgs-unstable.evcxr ];
     };
