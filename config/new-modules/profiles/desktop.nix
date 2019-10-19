ae5a6ba7fceff6695e64bb9850307e24858f4fe3
Evan Stoll <evanjsx@gmail.com>
2019-10-19 17:58:56 -0400

    gitkraken: get latest from channels

--- config/new-modules/profiles/desktop.nix
+++ config/new-modules/profiles/desktop.nix
@@ -57,7 +57,8 @@ in
       gparted
 
       # git ui
-      gitkraken
+      (pkgs.versions.latestVersion [pkgs.nixos-unstable.gitkraken pkgs.nixpkgs-unstable.gitkraken pkgs.stable.gitkraken])
+      
 
       # media
       feh
