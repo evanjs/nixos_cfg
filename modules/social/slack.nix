599803194af97f18e41020116dc6e1352e529da2
Evan Stoll <evanjsx@gmail.com>
2019-10-24 15:43:00 -0400

    slack: use latestVersion for slack to minimize dark theme issues

--- modules/social/slack.nix
+++ modules/social/slack.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    slack-dark
-    slack-term
+    ( pkgs.versions.latestVersion [ pkgs.slack-dark pkgs.nixos-unstable.slack-dark pkgs.nixos-unstable-small.slack-dark ] )
+    ( pkgs.versions.latestVersion [ pkgs.slack-term pkgs.nixos-unstable.slack-term pkgs.nixos-unstable-small.slack-term ] )
   ];
 }
