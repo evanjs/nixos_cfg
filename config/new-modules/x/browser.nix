6d936d68755b19418d79831d0f220c71583cb63c
Evan Stoll <evanjsx@gmail.com>
2019-10-31 13:48:08 -0400

    x/browser: enable WideVine for Chromium

--- config/new-modules/x/browser.nix
+++ config/new-modules/x/browser.nix
@@ -16,6 +16,10 @@ with lib;
   config = {
     environment.systemPackages = mkIf config.services.xserver.enable [ config.mine.chromium config.mine.firefox ];
     nixpkgs.config.firefox.enableBrowserpass = true;
+
+    # Needed for services that use DRM for streaming, such as VRV, Netflix, etc
+    nixpkgs.config.chromium.enableWideVine = true;
+    
     # Note: extensions don't work with -bin versions of firefox
     mine.firefox = pkgs.firefox;
     mine.chromium = pkgs.chromium;
