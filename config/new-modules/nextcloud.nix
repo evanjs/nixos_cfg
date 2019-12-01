5b40e2fa9003c7968f803da646d7b494fff23595
Evan Stoll <evanjsx@gmail.com>
2019-12-01 00:25:50 -0500

    nextcloud: reformat config

--- config/new-modules/nextcloud.nix
+++ config/new-modules/nextcloud.nix
@@ -9,40 +9,40 @@ in
   {
     options.mine.nextcloud = {
       enable = mkEnableOption "Nextcloud Configuration";
-        openFirewall = mkOption {
-          type = types.bool;
-          default = true;
-          example = false;
-          description = "Open ports in the firewall for NextCloud.  This is required for remote access.";
-        };
+      openFirewall = mkOption {
+        type = types.bool;
+        default = true;
+        example = false;
+        description = "Open ports in the firewall for NextCloud.  This is required for remote access.";
       };
+    };
 
     config = mkIf cfg.enable {
-          services.nextcloud = {
-            enable = true;
-            autoUpdateApps.enable = true;
-            caching.apcu = true;
-            config = {
-              adminpass = config.private.passwords.nextcloud;
-              dbtype = "pgsql";
-              extraTrustedDomains = [
-                "10.10.0.*"
-                "75.129.188.19"
-                "http://a28209498ca7.sn.mynetname.net"
-              ];
-            };
+      services.nextcloud = {
+        enable = true;
+        autoUpdateApps.enable = true;
+        caching.apcu = true;
+        config = {
+          adminpass = config.private.passwords.nextcloud;
+          dbtype = "pgsql";
+          extraTrustedDomains = [
+            "10.10.0.*"
+            "75.129.188.19"
+            "http://a28209498ca7.sn.mynetname.net"
+          ];
+        };
 
         home = "/data/web/nextcloud";
-            hostName = "nextcloud";
+        hostName = "nextcloud";
 
-            nginx.enable = true;
-          };
-        services.phpfpm.pools.nextcloud.phpOptions = ''
-            memcache.local = \OC\Memcache\APCu
-            apc.enable_cli = 1
-        '';
+        nginx.enable = true;
+      };
+      services.phpfpm.pools.nextcloud.phpOptions = ''
+        memcache.local = \OC\Memcache\APCu
+        apc.enable_cli = 1
+      '';
       environment.systemPackages = with pkgs; [
         p7zip
       ];
-      };
-    }
+    };
+  }
