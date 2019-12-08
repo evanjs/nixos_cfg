bacd26d99b587b86fda4958aaf0df8b75314a3f3
Evan Stoll <evanjsx@gmail.com>
2019-12-08 17:33:44 -0500

    fs: add thunderbolt package and udev rule

--- modules/fs.nix
+++ modules/fs.nix
@@ -3,4 +3,13 @@
   boot.extraModulePackages = [
     config.boot.kernelPackages.exfat-nofuse
   ];
+
+  # Thunderbolt
+  environment.systemPackages = with pkgs; [
+    thunderbolt
+  ];
+
+  services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
+  '';
 }
