c7152cff9c0734b577f811883930ba6f6c22e941
Evan Stoll <evan.stoll@rjginc.com>
2019-12-16 14:19:11 -0500

    slack: use normal slack - dark mode is now supported on desktop

--- modules/social/slack.nix
+++ modules/social/slack.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    ( pkgs.versions.latestVersion [ pkgs.slack-dark pkgs.nixos-unstable.slack-dark pkgs.nixos-unstable-small.slack-dark ] )
+    ( pkgs.versions.latestVersion [ pkgs.slack pkgs.nixos-unstable.slack pkgs.nixos-unstable-small.slack ] )
     ( pkgs.versions.latestVersion [ pkgs.slack-term pkgs.nixos-unstable.slack-term pkgs.nixos-unstable-small.slack-term ] )
   ];
 }
