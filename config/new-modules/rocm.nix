ca27c9f1371bbb21efaf99e38771aef0fe3c270d
Evan Stoll <evan.stoll@rjginc.com>
2020-01-05 01:44:14 -0500

    add rocm module

---
+++ config/new-modules/rocm.nix
@@ -0,0 +1,38 @@
+{ lib, pkgs, config, ... }:
+with lib;
+
+let cfg = config.mine.rocm;
+
+in {
+  options.mine.rocm = {
+    enable = mkEnableOption "Radeon Open Compute (2.10.0) packages for NixOS";
+
+    targets = mkOption {
+      type = types.nullOr (types.listOf types.str);
+      default = null;
+      example = [ "gfx803" "gfx900" "gfx906" ];
+      description = "The GPU compilation targets.";
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+    nixpkgs.overlays = [ (import "${(import ../sources).nixos-rocm}") ];
+
+    hardware.opengl.enable = true;
+    hardware.opengl.extraPackages = [ pkgs.rocm-opencl-icd ];
+
+    nixpkgs.config.rocmTargets = cfg.targets;
+
+    users.users = genAttrs config.mine.mainUsers (name: {
+      extraGroups = [ "video" ];
+    });
+
+    nix.sandboxPaths = [
+      "/dev/kfd"
+      "/sys/devices/virtual/kfd"
+      "/dev/dri/renderD128"
+    ];
+
+  };
+}
---
+++ config/sources/nixos-rocm.nix
@@ -0,0 +1,6 @@
+{ pkgs }: pkgs.fetchFromGitHub {
+  owner  = "nixos-rocm";
+  repo   = "nixos-rocm";
+  rev    = "7f9d0567c948b8f391ae1056ab03e73b26e9a851";
+  sha256 = "1zab4s2nd58a1kmb3jq1fw9gvc2axy0mck2y94ag95s85x6vvgx3";
+}
