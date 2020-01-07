69df7536e8ba60dd9187b1bc917b94eb7098ad21
Evan Stoll <evanjsx@gmail.com>
2020-01-07 11:44:24 -0500

    x/wm/xmonad.nix: nixfmt

--- config/new-modules/x/wm/xmonad.nix
+++ config/new-modules/x/wm/xmonad.nix
@@ -17,36 +17,40 @@ with lib;
         config = userConfig.config;
       };
 
-      mine.xmobar.enable = true;
-      mine.taffybar.enable = true;
-      mine.compton = {
+    mine.xmobar.enable = true;
+    mine.taffybar.enable = true;
+    mine.compton = {
+      enable = true;
+      highend = true;
+    };
+    mine.terminal.enable = true;
+
+    scripts = {
+      emacs = ''
+        if ! [ $(systemctl --user is-active emacs) = active ]; then
+        systemctl --user start emacs
+        fi
+        emacsclient -c -n
+      '';
+      xmobar = config.mine.xmobar.command;
+    };
+
+    mine.userConfig = { home.packages = [ pkgs.maim ]; };
+
+    mine.xUserConfig = {
+
+      xsession.windowManager.xmonad = {
         enable = true;
-        highend = true;
-      };
-      mine.terminal.enable = true;
-
-      scripts = {
-        emacs = ''
-            if ! [ $(systemctl --user is-active emacs) = active ]; then
-            systemctl --user start emacs
-            fi
-            emacsclient -c -n
-        '';
-        xmobar = config.mine.xmobar.command;
-      };
-
-      mine.userConfig = { home.packages = [ pkgs.maim ]; };
-
-      mine.xUserConfig = {
-
-        xsession.windowManager.xmonad = {
-          enable = true;
-          extraPackages = self: [ self.fuzzy config.mine.taffybar.package self.taffybar ];
-          enableContribAndExtras = true;
-          config = pkgs.runCommand "xmonad.hs" (config.scripts // {
+        extraPackages = self: [
+          self.fuzzy
+          config.mine.taffybar.package
+          self.taffybar
+        ];
+        enableContribAndExtras = true;
+        config = pkgs.runCommand "xmonad.hs" (config.scripts // {
           spacing = if config.mine.hardware.battery then "False" else "True";
-          }) "substituteAll ${./xmonad.hs} $out";
-          };
-          };
-          };
-          }
+        }) "substituteAll ${./xmonad.hs} $out";
+      };
+    };
+  };
+}
