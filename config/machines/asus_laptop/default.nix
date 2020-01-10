319a61788aecd6b650773a30a6d102aacf7929d2
Evan Stoll <evanjsx@gmail.com>
2020-01-09 22:10:22 -0500

    machines/asus_laptop: add scrape module

--- config/machines/asus_laptop/default.nix
+++ config/machines/asus_laptop/default.nix
@@ -12,6 +12,7 @@
     ../../../modules/db/postgresql.nix
     ../../../modules/development.nix
     ../../../modules/samba/client/home.nix
+    ../../../modules/scrape.nix 
     #../../../modules/channels.nix
   ];
 
