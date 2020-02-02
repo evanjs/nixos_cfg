018c2eb36e4eef3800e280d5250022f56ebe8f04
Evan Stoll <evanjsx@gmail.com>
2020-02-02 03:06:22 -0500

    xmobar: refactor

--- config/new-modules/x/xmobar.nix
+++ config/new-modules/x/xmobar.nix
@@ -4,6 +4,71 @@ with lib;
 
 let
   cfg = config.mine.xmobar;
+  scripts = {
+    xftFont = "xft:${config.mine.font.name}:size=8:bold:antialias=true";
+    power = ''
+        ${pkgs.bc}/bin/bc <<< "scale=1; $(cat /sys/class/power_supply/BAT0/current_now)/1000000"
+    '';
+    batt = ''
+      PATH="${pkgs.acpi}/bin:${pkgs.gawk}/bin:${pkgs.bc}/bin:$PATH"
+      battstat=$(acpi -b | cut -d' ' -f3 | tr -d ',')
+
+      charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
+      charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
+
+      charge=$(bc <<EOF
+      scale=2
+      100 * $charge_now / $charge_full
+      EOF
+      )
+
+      chargeInteger=$(printf "%.0f\n" "$charge")
+
+      if [ $chargeInteger -le 0 ]; then
+        chargeInteger=0
+      elif [ $chargeInteger -ge 100 ]; then
+        chargeInteger=100
+      fi
+
+      if [ $chargeInteger -le 12 ]; then
+        symbol=
+      elif [ $chargeInteger -le 37 ]; then
+        symbol=
+      elif [ $chargeInteger -le 62 ]; then
+        symbol=
+      elif [ $chargeInteger -le 87 ]; then
+        symbol=
+      else
+        symbol=
+      fi
+
+      red=$(( 255 - $chargeInteger * 255 / 100 ))
+      green=$(( $chargeInteger * 255 / 100 ))
+
+      case $battstat in
+      Full)
+      ;;
+      Discharging)
+      postfix="-$(date -u -d $(acpi -b | cut -d' ' -f5) +"%Hh%M")"
+      ;;
+      Charging)
+      postfix="+$(date -u -d $(acpi -b | cut -d' ' -f5) +"%Hh%M")"
+      ;;
+      *)
+      ;;
+      esac
+
+      printf "<fc=#%02x%02x00>%s%% %s</fc> (%s)\n" "$red" "$green" "$charge" "$symbol" "$postfix"
+    '';
+    playing = ''
+      status="$(systemctl --user is-active music)"
+      if [ $status = active ]; then
+        echo 
+      else
+        echo 
+      fi
+    '';
+  };
   configFile = let
     fonts = lib.concatStringsSep "," [
       "Helvetica Neue LT Std,HelveticaNeueLT Std Lt Cn:style=47 Light Condensed,Regular:pixelsize=12"
@@ -16,7 +81,7 @@ let
     Config {
 
     -- appearance
-      font =         "xft:FiraCode Mono:size=8:bold:antialias=true"
+      font =         "@xftFont@"
     , bgColor = "#2b2b29"
     , fgColor = "#c3ae93"
     , alpha = 210
@@ -27,7 +92,7 @@ let
     -- layout
     , sepChar =  "%"   -- delineator between plugin names and straight text
     , alignSep = "}{"  -- separator between left-right alignment
-    , template = "%StdinReader% }{ %battery% | %multicpu% | %coretemp% | %memory% | %default:Master% | %dynnetwork% | %KTVC% | %date% || %kbd% "
+    , template = "%StdinReader%}{%battery%|%multicpu%|%coretemp%|%memory%|%default:Master%|%dynnetwork%|%KTVC%|%date%||%kbd% "
 
    -- Southwest Michigan Regional Airport (KBEH)
    -- , template = "%StdinReader% }{ %battery% | %multicpu% | %coretemp% | %memory% | %default:Master% | %dynnetwork% | %KBEH% | %date% || %kbd% "
@@ -57,7 +122,7 @@ let
     , commands =
 
     -- weather monitor
-    [ Run Weather "KTVC" [ "--template", "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><tempF></fc>°F | <fc=#4682B4><rh></fc>%"
+    [ Run Weather "KTVC" [ "--template", "<skyCondition> | <fc=#4FC><tempC></fc><fc=#FFF>°C</fc> | <fc=#4FC><tempF></fc><fc=#FFF>°F</fc> | <fc=#4FC><rh></fc><fc=#FFF>%</fc>"
     ] 36000
 
       -- network activity monitor (dynamic interface resolution)
@@ -130,82 +195,15 @@ let
   '';
 in {
 
-  options.mine.xmobar = {
-    command = mkOption {
-      description = "The command to execute for xmobar";
-      default = "${pkgs.haskellPackages.xmobar}/bin/xmobar ${configFile} $@";
+  options.mine.xmobar =
+    let
+      renderedConfig = pkgs.runCommand "xmobar-config" scripts "substituteAll ${configFile} $out";
+    in
+    {
+      command = mkOption {
+        description = "The command to execute for xmobar";
+        default = "${pkgs.haskellPackages.xmobar}/bin/xmobar ${renderedConfig} $@";
+      };
+      enable = mkEnableOption "xmobar config";
     };
-    enable = mkEnableOption "xmobar config";
-  };
-
-  config = mkIf cfg.enable {
-
-    scripts = {
-
-      power = ''
-        ${pkgs.bc}/bin/bc <<< "scale=1; $(cat /sys/class/power_supply/BAT0/current_now)/1000000"
-      '';
-      batt = ''
-        PATH="${pkgs.acpi}/bin:${pkgs.gawk}/bin:${pkgs.bc}/bin:$PATH"
-
-        battstat=$(acpi -b | cut -d' ' -f3 | tr -d ',')
-
-        charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
-        charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
-
-        charge=$(bc <<EOF
-        scale=2
-        100 * $charge_now / $charge_full
-        EOF
-        )
-
-        chargeInteger=$(printf "%.0f\n" "$charge")
-
-
-        if [ $chargeInteger -le 0 ]; then
-          chargeInteger=0
-        elif [ $chargeInteger -ge 100 ]; then
-          chargeInteger=100
-        fi
-
-        if [ $chargeInteger -le 12 ]; then
-          symbol=
-        elif [ $chargeInteger -le 37 ]; then
-          symbol=
-        elif [ $chargeInteger -le 62 ]; then
-          symbol=
-        elif [ $chargeInteger -le 87 ]; then
-          symbol=
-        else
-          symbol=
-        fi
-
-        red=$(( 255 - $chargeInteger * 255 / 100 ))
-        green=$(( $chargeInteger * 255 / 100 ))
-
-        case $battstat in
-        Full)
-          ;;
-        Discharging)
-          postfix="-$(date -u -d $(acpi -b | cut -d' ' -f5) +"%Hh%M")"
-          ;;
-        Charging)
-          postfix="+$(date -u -d $(acpi -b | cut -d' ' -f5) +"%Hh%M")"
-          ;;
-        *)
-          ;;
-        esac
-
-        printf "<fc=#%02x%02x00>%s%% %s</fc> (%s)\n" "$red" "$green" "$charge" "$symbol" "$postfix"
-      '';
-      playing = ''
-        status="$(systemctl --user is-active music)"
-        if [ $status = active ]; then
-          echo 
-        else
-          echo 
-        fi
-      '';
-    };
-  };
-}
+  }
