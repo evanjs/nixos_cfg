11e184675c04cb00713cb81f39e71fbc9b5e37f2
Evan Stoll <evanjsx@gmail.com>
2020-02-23 15:13:51 -0500

    sources: add nur-combined

---
+++ config/sources/nur-combined.nix
@@ -0,0 +1,6 @@
+{ fetchFromGitHub }: fetchFromGitHub {
+  owner = "nix-community";
+  repo = "nur-combined";
+  rev = "7317ece7f31867ecb90df80252f4f1ef65664d17";
+  sha256 = "0nk32zl5agxmv4kmbb20rgrdkvi3z50yw9mzfj2kpvy5b2zg3xjc";
+}
