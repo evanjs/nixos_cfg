bdf4b7cf3603022d3bf79456302f535762c94652
Evan Stoll <evanjsx@gmail.com>
2020-02-25 11:56:22 -0500

    cachix: add configuration for rjg.cachix.org

---
+++ config/cachix/rjg.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://rjg.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "rjg.cachix.org-1:BpJV6KZV+bEBhzHAlC7Nw9UrzEDJQnkkSBqBsFRNTW0="
+    ];
+  };
+}
+    
