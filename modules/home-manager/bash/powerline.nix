abc0e74ab2174a0994740420928388a6a586eb46
Evan Stoll <evanjsx@gmail.com>
2020-05-16 19:04:54 -0400

    home-manager/bash: disable powerline-go

--- modules/home-manager/bash/powerline.nix
+++ modules/home-manager/bash/powerline.nix
@@ -4,6 +4,9 @@
     home.packages = with pkgs; [
       powerline-go
     ];
+
+    # TODO: Can this be disabled for nix-shells?
+    # `set -x` makes nix-shells difficult to use when using powerline
     programs.bash.initExtra = ''
       function _update_ps1() {
       PS1="$(${pkgs.powerline-go}/bin/powerline-go -error $?)"
