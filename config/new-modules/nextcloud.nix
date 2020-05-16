8ec7db37eff6e80fc1c9e8af95b6f35885e7d16e
Evan Stoll <evanjsx@gmail.com>
2020-05-16 14:52:23 -0400

    nextcloud: format with nixpkgs-fmt

--- config/new-modules/nextcloud.nix
+++ config/new-modules/nextcloud.nix
@@ -20,46 +20,45 @@ in
   config = mkMerge [
     (
       {
-    services.nextcloud = {
-      enable = true;
-      autoUpdateApps.enable = true;
-      caching.apcu = true;
-      config = {
-        adminpass = config.private.passwords.nextcloud;
-        dbtype = "pgsql";
-        extraTrustedDomains = [
-          "10.10.0.*"
-          "75.129.188.19"
-          "http://a28209498ca7.sn.mynetname.net"
-        ];
-      };
+        services.nextcloud = {
+          enable = true;
+          autoUpdateApps.enable = true;
+          caching.apcu = true;
+          config = {
+            adminpass = config.private.passwords.nextcloud;
+            dbtype = "pgsql";
+            extraTrustedDomains = [
+              "10.10.0.*"
+              "75.129.188.19"
+              "http://a28209498ca7.sn.mynetname.net"
+            ];
+          };
 
-      home = "/data/web/nextcloud";
-      hostName = "nextcloud";
-      maxUploadSize = "4096M";
+          home = "/data/web/nextcloud";
+          hostName = "nextcloud";
+          maxUploadSize = "4096M";
 
-      nginx.enable = true;
-      package = pkgs.nextcloud18;
-    };
-    services.phpfpm.pools.nextcloud.phpOptions = ''
-      memcache.local = \OC\Memcache\APCu
-      apc.enable_cli = 1
-    '';
+          nginx.enable = true;
+          package = pkgs.nextcloud18;
+        };
+        services.phpfpm.pools.nextcloud.phpOptions = ''
+          memcache.local = \OC\Memcache\APCu
+          apc.enable_cli = 1
+        '';
 
-    environment.systemPackages = with pkgs; [ p7zip unrar ];
+        environment.systemPackages = with pkgs; [ p7zip unrar ];
       }
     )
 
-    (
-      (mkIf (cfg.enable && cfg.aria2.enable)) {
-        services.aria2 = {
-          enable = true;
-          openPorts = true;
-          extraArguments = "--rpc-allow-origin-all -c -D --check-certificate=false --save-session-interval=2 --continue=true --rpc-save-upload-metadata=true --force-save=true --log-level=warn --rpc-listen-all=false";
-  };
+    ( (mkIf (cfg.enable && cfg.aria2.enable)) {
+      services.aria2 = {
+        enable = true;
+        openPorts = true;
+        extraArguments = "--rpc-allow-origin-all -c -D --check-certificate=false --save-session-interval=2 --continue=true --rpc-save-upload-metadata=true --force-save=true --log-level=warn --rpc-listen-all=false";
+      };
 
-        users.users.nextcloud.extraGroups = [ "aria2" ];
-      }
+      users.users.nextcloud.extraGroups = [ "aria2" ];
+    }
     )
   ];
 }
