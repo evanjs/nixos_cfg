a121b9443cbfeb6c4efa2bbbe4511c8d023de718
Evan Stoll <evanjsx@gmail.com>
2020-05-29 12:49:18 -0400

    Initial work to make more items work with standalone home-manager

---
+++ config/machines/non_nixos/wgrig/home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports = [
+    ../../../new-modules/vim/vim.nix
+    ../../../new-modules/zsh-home-manager.nix
+  ];
+
+  home.packages = with pkgs; [
+    myNeovim
+    ripgrep
+    bat
+    fd
+    du-dust
+    nixpkgs-fmt
+    git
+  ];
+
+  programs = {
+    bash.enableAutojump = true;
+    neovim = {
+      enable = true;
+      package = pkgs.myNeovim;
+      viAlias = true;
+    };
+    zsh = {
+      enable = true;
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+}
