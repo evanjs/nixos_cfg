3682c650045368ab0106a30d7df37ea6d589d346
Evan Stoll <evanjsx@gmail.com>
2020-07-05 17:08:57 -0400

    modules/languages/python: don't add interpreters to systemPackages
    - remove pillow from systemPackages
    - remove setuptools from systemPackages

--- modules/languages/python/default.nix
+++ modules/languages/python/default.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    python37Full
-    python38Full
-    python37Packages.pillow
-    python37Packages.setuptools
     pipenv
   ];
 }
