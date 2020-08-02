bbe1dc6c18bcedbd1e9dcfa314ac1c981745c01b
Evan Stoll <evanjsx@gmail.com>
2020-08-02 14:52:28 -0400

    overlays/libs: add functions for finding drvs within attrs or [attrs]

---
+++ overlays/libs/helpers.nix
@@ -0,0 +1,12 @@
+{ lib, ... }:
+with lib;
+{
+
+  # TODO: Improve error-handling/provide error messages that specify the expected inputs for each function, etc
+
+  # attrContainsDrv :: attrs -> string -> listOf Derivation
+  attrsContainsDrv = attrs: query: concatStringsSep " " (attrNames (lib.filterAttrs (x: y: lib.strings.hasInfix query (lib.strings.getName x)) attrs));
+  
+  # attrsListContainsDrv :: [attrs] -> string -> listOf Derivation
+  attrsListContainsDrv = attrsList: query: concatStringsSep " " (map (drv: strings.getName drv) (lib.filter (x: lib.strings.hasInfix query (lib.strings.getName x)) attrsList));
+}
