36401b262cbbfa4aa4bd928f5d757d092cede852
Evan Stoll <evanjsx@gmail.com>
2020-09-19 20:05:04 -0400

    terminal: use kitty option from home-manager
    -

--- config/new-modules/x/terminal/default.nix
+++ config/new-modules/x/terminal/default.nix
@@ -1,45 +1,40 @@
 { pkgs, lib, config, ... }:
 
 with lib;
-
 let
 
   cfg = config.mine.terminal;
 
 in
-
 {
 
   options.mine.terminal = {
-
     enable = mkEnableOption "My terminal";
-
     binary = mkOption {
       type = types.path;
       description = "Path to terminal binary";
     };
-
   };
 
-  config = mkIf cfg.enable {
+  config =
+    let
+      kitty = lib.findFirst (p: (lib.getName p) == "kitty") { } config.mine.xUserConfig.home.packages;
+    in
+    mkIf cfg.enable {
 
-    mine.terminal.binary = "${pkgs.kitty}/bin/kitty";
+      mine.terminal.binary = "${kitty}/bin/kitty";
 
     environment.systemPackages = [
-      pkgs.kitty
       pkgs.tmux
       pkgs.screen
     ];
 
-    mine.xUserConfig =
-      let
-        scripts = {
-          fontName = config.mine.font.name;
+      mine.xUserConfig = {
+        programs.kitty = {
+          enable = true;
+          font.name = "${config.mine.font.name}";
+          font.package = config.mine.font.package;
         };
-      in
-      {
-
-      xdg.configFile."kitty/kitty.conf".source = pkgs.runCommand "kitty.conf" scripts "substituteAll ${./kitty.conf} $out";
 
       home.file.".tmux.conf".text = ''
         set -g default-terminal "xterm-256color"
