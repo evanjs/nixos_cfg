b39ae4a30a82ba201a5f6b5ed7fd607cc7743e8e
Evan Stoll <evanjsx@gmail.com>
2021-06-23 23:02:39 -0400

    mcdex: formatting, update vendored sources hash
    ## mcdex
    * remove deps.nix from old go module helper invocation
    * update vendored sources hash

--- overlays/evanjs/pkgs/games/mcdex/default.nix
+++ overlays/evanjs/pkgs/games/mcdex/default.nix
@@ -1,4 +1,4 @@
-{ buildGoModule, fetchFromGitHub, lib }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   name = "mcdex-unstable-2021-01-31";
@@ -12,15 +12,15 @@ buildGoModule rec {
     sha256 = "0d5mqvzk9b47kn19cxqbmn8j4792h2yfyj0qpbrksdrwy6a2gg3b";
   };
 
-  vendorSha256 = "sha256-OIJzhQ5cdFizKipjYhTh+rpkAy3OyVgCHMmOdLPPs78=";
+  vendorSha256 = "sha256-Z1kJOwrUKHk2XQL7MY5STuvHSsIRTdloJWg6bNQQ7UE=";
 
   # TODO: add metadata https://nixos.org/nixpkgs/manual/#sec-standard-meta-attributes
   meta = {
     license = lib.licenses.asl20;
     description = "Minecraft Modpack Management";
     longDescription = ''
-      mcdex is a command-line utility that runs on Linux, Windows and OSX
-      and makes it easy to manage your modpacks while using the native Minecraft launcher.
+      mcdex is a command-line utility that runs on Linux, Windows and OSX and makes it easy
+      to manage your modpacks while using the native Minecraft launcher.
     '';
   };
 }
