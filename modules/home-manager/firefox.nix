5fdd13e013704f7283fc36b871abf91dfc9201f1
Evan Stoll <evanjsx@gmail.com>
2024-02-15 12:11:20 -0500

    home-manager/firefox: remove `https-everywhere` extension; format

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -7,11 +7,14 @@
   home-manager.users.evanjs = {
     programs.firefox = {
       enable = lib.mkDefault true;
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      https-everywhere
-      ublock-origin
-      reddit-enhancement-suite
-      ];
+      profiles = {
+        mine = {
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            reddit-enhancement-suite
+          ];
+        };
+      };
     };
   };
 }
