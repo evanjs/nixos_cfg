5b20c467a66d3e14dcc2cf029bbf6a9ff599ccd5
Evan Stoll <evanjsx@gmail.com>
2024-02-15 12:20:29 -0500

    machines/wdesk: apply staged changes

--- config/machines/wdesk/default.nix
+++ config/machines/wdesk/default.nix
@@ -27,18 +27,37 @@
 
   mine.enableUser = true;
   mine.profiles.desktop.enable = true;
+  mine.emacs.enable = lib.mkForce false;
+  mine.taffybar.enable = false;
 
-  mine.android.enable = true;
+  mine.android.enable = false;
+  home-manager.users.evanjs.programs.mbsync.enable = false;
+  home-manager.users.evanjs.services.mbsync.enable = false;
 
   # enable after rocm packages are locked to stable channel
-  mine.rocm.enable = false;
+  mine.rocm.enable = true;
 
   mine.virtualization.virtualbox.enable = false;
   boot.kernelPackages = lib.mkForce pkgs.stable.linuxPackages_latest;
 
+
+  services.xserver.displayManager.gdm = {
+    wayland = false;
+  };
+  
+  systemd.targets."multi-user".conflicts = [ "getty@tty7.service" ];
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
   networking.hostName = "sekka";
+
+  #services.tailscale.enable = true;
 
   boot.initrd.checkJournalingFS = false;
 
+  home-manager.users.evanjs.home.stateVersion = "22.05";
+  home-manager.users.root.home.stateVersion = "22.05";
+  mine.xUserConfig.home.stateVersion = "22.05";
   system.stateVersion = "19.09";
 }
