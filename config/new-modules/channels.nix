52ebcf9e08c49a39a7fc906ff0c899df8581b50b
Evan Stoll <evanjsx@gmail.com>
2025-02-19 12:34:42 -0500

    channels.nix: format file

--- config/new-modules/channels.nix
+++ config/new-modules/channels.nix
@@ -1,5 +1,11 @@
-{ config, pkgs, lib, ... }:
-with lib; {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+{
 
   config.nixpkgs.config = {
     # Allow proprietary packages
@@ -7,33 +13,33 @@ with lib; {
 
     # Create an alias for the unstable channel
     packageOverrides = pkgs: rec {
-      stable = import (fetchTarball
-        "https://nixos.org/channels/nixos-24.11/nixexprs.tar.xz") {
+      stable = import (fetchTarball "https://nixos.org/channels/nixos-24.11/nixexprs.tar.xz") {
         #stable = import <nixos-24.11> {
         config = config.nixpkgs.config;
       };
-       nixos-unstable = import (fetchTarball
-         "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {
-          #nixos-unstable = import <nixos-unstable> {
-          # pass the nixpkgs config to the unstable alias
-          # to ensure `allowUnfree = true;` is propagated:
-          config = config.nixpkgs.config;
-        };
+      nixos-unstable = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {
+        #nixos-unstable = import <nixos-unstable> {
+        # pass the nixpkgs config to the unstable alias
+        # to ensure `allowUnfree = true;` is propagated:
+        config = config.nixpkgs.config;
+      };
 
-       nixpkgs-unstable = import (fetchTarball
-         "https://github.com/nixos/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz") {
-          #nixpkgs-unstable = import <nixpkgs-unstable> {
-          # pass the nixpkgs config to the unstable alias
-          # to ensure `allowUnfree = true;` is propagated:
-          config = config.nixpkgs.config;
-        };
-       nixos-unstable-small = import (fetchTarball
-         "https://nixos.org/channels/nixos-unstable-small/nixexprs.tar.xz") {
-          #nixos-unstable-small = import <nixos-unstable-small> {
-          # pass the nixpkgs config to the unstable alias
-          # to ensure `allowUnfree = true;` is propagated:
-          config = config.nixpkgs.config;
-        };
+      nixpkgs-unstable =
+        import (fetchTarball "https://github.com/nixos/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz")
+          {
+            #nixpkgs-unstable = import <nixpkgs-unstable> {
+            # pass the nixpkgs config to the unstable alias
+            # to ensure `allowUnfree = true;` is propagated:
+            config = config.nixpkgs.config;
+          };
+      nixos-unstable-small =
+        import (fetchTarball "https://nixos.org/channels/nixos-unstable-small/nixexprs.tar.xz")
+          {
+            #nixos-unstable-small = import <nixos-unstable-small> {
+            # pass the nixpkgs config to the unstable alias
+            # to ensure `allowUnfree = true;` is propagated:
+            config = config.nixpkgs.config;
+          };
     };
   };
 }
