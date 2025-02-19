b433f978d340430548ee0cb9090f4b4ac0e96fb7
Evan Stoll <evanjsx@gmail.com>
2025-02-19 11:48:09 -0500

    nur: remove config import

--- modules/nur.nix
+++ modules/nur.nix
@@ -5,7 +5,6 @@
     packageOverrides = pkgs: {
       nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
         inherit pkgs;
-        #config = config.nixpkgs.config;
       };
     };
   };
