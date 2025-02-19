45c23587b2d870509de915e36003516ac78390de
Evan Stoll <evanjsx@gmail.com>
2025-02-19 12:33:10 -0500

    channels: update
    - stable: 23.11 -> 24.11
    - use URLs instead of channel names

--- config/new-modules/channels.nix
+++ config/new-modules/channels.nix
@@ -7,26 +7,29 @@ with lib; {
 
     # Create an alias for the unstable channel
     packageOverrides = pkgs: rec {
-
       stable = import (fetchTarball
-        "https://nixos.org/channels/nixos-23.11/nixexprs.tar.xz") {
-          config = config.nixpkgs.config;
-        };
-      nixos-unstable = import (fetchTarball
-        "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {
+        "https://nixos.org/channels/nixos-24.11/nixexprs.tar.xz") {
+        #stable = import <nixos-24.11> {
+        config = config.nixpkgs.config;
+      };
+       nixos-unstable = import (fetchTarball
+         "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {
+          #nixos-unstable = import <nixos-unstable> {
           # pass the nixpkgs config to the unstable alias
           # to ensure `allowUnfree = true;` is propagated:
           config = config.nixpkgs.config;
         };
 
-      nixpkgs-unstable = import (fetchTarball
-        "https://github.com/nixos/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz") {
+       nixpkgs-unstable = import (fetchTarball
+         "https://github.com/nixos/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz") {
+          #nixpkgs-unstable = import <nixpkgs-unstable> {
           # pass the nixpkgs config to the unstable alias
           # to ensure `allowUnfree = true;` is propagated:
           config = config.nixpkgs.config;
         };
-      nixos-unstable-small = import (fetchTarball
-        "https://nixos.org/channels/nixos-unstable-small/nixexprs.tar.xz") {
+       nixos-unstable-small = import (fetchTarball
+         "https://nixos.org/channels/nixos-unstable-small/nixexprs.tar.xz") {
+          #nixos-unstable-small = import <nixos-unstable-small> {
           # pass the nixpkgs config to the unstable alias
           # to ensure `allowUnfree = true;` is propagated:
           config = config.nixpkgs.config;
