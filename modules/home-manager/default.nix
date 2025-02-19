36ca35241736b84835998db23840d081416a6f3b
Evan Stoll <evanjsx@gmail.com>
2025-02-19 12:24:09 -0500

    Use niv pin for home-manager instead of nur

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 let
   sources = import ../../config/nix/sources.nix {};
-  nurNoPkgs = import sources.nur {};
+  home-manager = import sources.home-manager {};
 in
   {
   # Import home-manager for use as a NixOS submodule
   imports = [
-    nurNoPkgs.repos.rycee.modules.home-manager
+    <home-manager/nixos>
     ./home.nix
   ];
 
